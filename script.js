f0e38974c28c2a72aaa425afa0e7ea399f56f6f8
dk99kodehode <dk99kodehode@gmail.com>
2026-07-10 05:46:07 +0200

    some changes to js

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 // Henter ut elementer fra ID
 const inputGuess = document.getElementById("input-guess");
-const randomJoke = document.getElementById("joke");
+const jokeText = document.getElementById("joke");
 const submitGuess = document.getElementById("submit-guess");
+
+const correctPopup = document.getElementById("correct");
+const wrongPopup = document.getElementById("wrong");
 
 fetchData();
 
 async function fetchData() {
   try {
-    const response = await fetch(
-      "https://official-joke-api.appspot.com/random_joke",
-    );
+    const response = await fetch("https://v2.jokeapi.dev/joke/Any?amount=10");
 
     if (!response.ok) {
       throw new Error("Couldnt fetch");
     }
-    const data = await response.json();
 
-    randomJoke.textContent = data.setup;
+    // velger et random nummer i fra data amount som nå er 10, hardcoded i fetch linken.
+    const data = await response.json();
+    const randomJoke = Math.floor(Math.random() * data.amount);
+
+    jokeText.textContent = data.jokes[randomJoke].setup;
+    const currentJoke = data.jokes[randomJoke];
 
     submitGuess.addEventListener("click", () => {
-      if (inputGuess.value === data.punchline) {
-        alert("Correct");
+      // må bruke punchline / delivery på noen de har forskjellige struktur
+      if (
+        inputGuess.value === currentJoke.punchline ||
+        inputGuess === currentJoke.delivery
+      ) {
+        correctPopup.style.display = "block";
       } else {
-        alert("Wrong");
+        wrongPopup.style.display = "block";
       }
     });
   } catch (error) {
